bb3b8373bc1402ef560426a4ec2ca4d39672bffa
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-03-08 19:37:03 +0600

    /promotions/:promoId endpoint done

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -25,4 +25,22 @@ promoRouter.route('/')
     res.end('Deleting all promotions');
 });
 
+promoRouter.route('/:promotionId')
+.get((req, res, next) => {
+    res.end('Will send details of the promotion: ' + req.params.promotionId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotion/' + req.params.promotionId);
+})
+.put((req, res, next) => {
+    res.write('Updating the promotion: ' + req.params.promotionId + '\n');
+    res.end('Will update the promotion: ' + req.body.name +
+        ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting promotion: ' + req.params.promotionId);
+});
+
+
 module.exports = promoRouter;
