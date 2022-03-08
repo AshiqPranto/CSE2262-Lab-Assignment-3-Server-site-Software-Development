75b3a0360eee476700b7c55d3254c7275e481a54
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-03-08 19:31:39 +0600

    /leaders/:leaderId endpoint done

--- routes/leadersRouter.js
+++ routes/leadersRouter.js
@@ -27,4 +27,22 @@ leadersRouter.route('/')
 });
 
 
+leadersRouter.route('/:leaderId')
+.get((req, res, next) => {
+    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /leader/' + req.params.leaderId);
+})
+.put((req, res, next) => {
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name +
+        ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+
 module.exports = leadersRouter;
