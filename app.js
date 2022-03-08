ab3d5f54781d0dd423b9bdc8308390e71ed5002f
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-03-08 19:35:13 +0600

    /promotions endpoint done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leadersRouter = require('./routes/leadersRouter');
+var promoRouter = require('./routes/promoRouter');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/leaders',leadersRouter);
+app.use('/promotions',promoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
